744ed7a887ad1d68bd9c10145b89db8d573f0d74
Kaibin <ekaibwu@gmail.com>
2013-12-13 00:12:34 +0800

    add vendor

---
+++ Core/Application/UINavigationController+Retro.h
@@ -0,0 +1,16 @@
+//
+//  UINavigationController+Retro.h
+//  Letao
+//
+//  Created by kaibin on 13-12-8.
+//  Copyright (c) 2013年 Kaibin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UINavigationController (Retro)
+
+- (void)pushViewControllerRetro:(UIViewController *)viewController;
+- (void)popViewControllerRetro;
+
+@end
---
+++ Core/Application/UINavigationController+Retro.m
@@ -0,0 +1,37 @@
+//
+//  UINavigationController+Retro.m
+//  Letao
+//
+//  Created by kaibin on 13-12-8.
+//  Copyright (c) 2013年 Kaibin. All rights reserved.
+//
+
+#import "UINavigationController+Retro.h"
+
+#import "UINavigationController+Retro.h"
+
+@implementation UINavigationController (Retro)
+
+- (void)pushViewControllerRetro:(UIViewController *)viewController {
+    CATransition *transition = [CATransition animation];
+    transition.duration = 0.25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromRight;
+    [self.view.layer addAnimation:transition forKey:nil];
+    
+    [self pushViewController:viewController animated:NO];
+}
+
+- (void)popViewControllerRetro {
+    CATransition *transition = [CATransition animation];
+    transition.duration = 0.25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromLeft;
+    [self.view.layer addAnimation:transition forKey:nil];
+    
+    [self popViewControllerAnimated:NO];
+}
+
+@end
